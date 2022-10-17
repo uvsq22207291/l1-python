18fc3e92bf887f010428ca30776de54d5ee20642
uvsq22207291 <114353771+uvsq22207291@users.noreply.github.com>
2022-10-17 09:45:58 +0200

    Update td3.py

--- exercises/TD03_fonctions/td3.py
+++ exercises/TD03_fonctions/td3.py
@@ -1,5 +1,4 @@
-#temps[0] : jours, temps[1]: heures, temps[2]: minutes, temps[3]: secondes
-
+#Question 1
 def tempsEnSeconde(temps):
     """ Renvoie la valeur en seconde de temps donné comme jour, heure, minute, seconde."""
     jours_en_secondes = temps[0] * 86400
@@ -11,4 +10,35 @@ def tempsEnSeconde(temps):
 
 temps = (3,23,1,34)
 print(type(temps))
-print(tempsEnSeconde(temps))
+print(tempsEnSeconde(temps)) 
+
+#Question 2
+def secondeEnTemps(seconde):
+    """Renvoie le temps (jour, heure, minute, seconde) qui correspond au nombre de seconde passé en argument"""
+    minutes = seconde // 60
+    seconde %= 60
+    heures = minutes // 60
+    minutes %= 60
+    jours = heures // 24
+    heures %= 24
+    pass
+    return (jours, heures, minutes,seconde)
+
+temps = secondeEnTemps(10000)
+print(temps[0],"jours",temps[1],"heures",temps[2],"minutes",temps[3],"secondes")
+
+#Question 3
+
+def afficheTemps(temps):
+    if jours and heures and minutes and secondes > 1 :
+        print ('jours', 'heures', 'minutes', 'secondes')
+    jours = afficheTemps[0]
+    heures = afficherTemps[1]
+    minutes = afficherTemps[2]
+    secondes = afficherTemps[3]
+    return (jours,heures,minutes,secondes)
+    
+    pass
+print (afficheTemps(temps))
+afficheTemps = (1,0,14,23) 
+#print (afficheTemps(temps))
