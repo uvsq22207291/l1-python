b1815f71d8616f6245d2b358c5ef0e5a623b4a26
Philcent GUIAO <philcent.guiao@ens.uvsq.fr>
2022-11-20 22:57:58 +0100

    Ajout exercice noté

--- exercises/TD04_listes/carre_magique.py
+++ exercises/TD04_listes/carre_magique.py
@@ -6,12 +6,18 @@ carre_mag = [[0] * 4 for i in range(4)]
 for i in range(4) :
     for j in range(4) :
         if i == j :
-            carre_mag[i][j] = 1
+            carre_mag [i][j] = 1
         elif i < j :
             carre_mag[i][j] = 2
             
 for i in carre_mag:
     print(' '.join([str(j) for j in i]))
+
+nombres = [4,14,15,1,9,7,6,12,5,11,10,8,16,2,3,13]
+print(nombres[0:4])
+print(nombres[4:8])
+print(nombres[8:12])
+print(nombres[12:16])
 
 #Question 3
 carre_pas_mag = [[0] * 4 for i in range(4)]
@@ -24,3 +30,9 @@ for i in range(4) :
             
 for i in carre_pas_mag:
     print(' '.join([str(j) for j in i]))
+
+nombres = [4,14,15,1,9,3,6,12,5,11,10,8,16,2,3,13]
+print(nombres[0:4])
+print(nombres[4:8])
+print(nombres[8:12])
+print(nombres[12:16])
