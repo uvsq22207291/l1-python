08a7d3c16f5c9e75974d30e1010d79587defcb89
Philcent GUIAO <philcent.guiao@ens.uvsq.fr>
2022-12-04 22:29:46 +0100

    TD4 LISTES

--- exercises/TD04_listes/carre_magique.py
+++ exercises/TD04_listes/carre_magique.py
@@ -1,112 +1,70 @@
-#Question 1
-# Il s'agit bien d'un carrée magique car la somme des nombres de chaque rangée, colonne et diagonale principale est égale. La constante magique est 34.
+#Question2
+carre_mag = [[4,14,15,1],[9,7,6,12],[5,11,10,8],[16,2,3,13]]
 
-#Question 2
-carre_mag = [[0] * 4 for i in range(4)]
-for i in range(4) :
-    for j in range(4) :
-        if i == j :
-            carre_mag [i][j] = 1
-        elif i < j :
-            carre_mag[i][j] = 2
-            
-for i in carre_mag:
-    print(' '.join([str(j) for j in i]))
-#OU
-nombres = [4,14,15,1,9,7,6,12,5,11,10,8,16,2,3,13]
-print(nombres[0:4])
-print(nombres[4:8])
-print(nombres[8:12])
-print(nombres[12:16])
-#OU
-carre_mag = [
-    [4,14,15,1],
-    [9,7,6,12],
-    [5,11,10,8],
-    [16,2,3,13]
-]
+#Question3
+carre_pas_mag = []
+for lignes in carre_mag:
+  carre_pas_mag.append(lignes.copy())
+carre_pas_mag [3][2] = 7
 
-for i in range (len(carre_mag)) :
-    for j in range (len(carre_mag[i])) :
-        print (carre_mag[i][j], end= " ")
-    print ()
+#Question4
+def affichecarre(carre):
+  for i in carre :
+    print (i)
+  print ()
+affichecarre(carre_mag)
+affichecarre(carre_pas_mag)
 
-#Question 3
-carre_pas_mag = [[0] * 4 for i in range(4)]
-for i in range(4) :
-    for j in range(4) :
-        if i == j :
-            carre_pas_mag[i][j] = 4
-        elif i < j :
-            carre_pas_mag[i][j] = 2
-            
-for i in carre_pas_mag:
-    print(' '.join([str(j) for j in i]))
+#Question5
+def testlignesegales(carre):
+  somme = sum(carre[0])
+  for lignes in carre :
+    if sum(lignes) != somme :
+      return -1
+  return somme
+print (testlignesegales(carre_mag))
+print (testlignesegales(carre_pas_mag))
 
-nombres = [4,14,15,1,9,3,6,12,5,11,10,8,16,2,3,13]
-print(nombres[0:4])
-print(nombres[4:8])
-print(nombres[8:12])
-print(nombres[12:16])
-#OU
+#Question6
+def testcolonnes(carre):
+  colonne_1 = [lignes[0] for lignes in carre]
+  somme = sum (colonne_1)
+  for j in range (1, len(carre)):
+    colonne = [lignes[j] for lignes in carre]
+    if sum(colonne) != somme :
+      return -1
+  return somme
+print(testcolonnes(carre_mag))
+print(testcolonnes(carre_pas_mag))
 
-carre_non_mag = [
-    [4,14,15,1],
-    [9,3,6,12],
-    [5,11,10,8],
-    [16,2,3,13]
-]
+#Question7
+def testdiagonalesegales(carre):
+  taille = len(carre)
+  diagonale_1 = [carre[i][i] for i in range (taille)]
+  diagonale_2 = [carre[i][taille - i - 1] for i in range (taille)]
+  somme = sum(diagonale_1)
+  if sum (diagonale_2) != somme :
+    return -1
+  else :
+    return somme
+print(testdiagonalesegales(carre_mag))
+print(testdiagonalesegales(carre_pas_mag))
 
-for i in range (len(carre_non_mag)) :
-    for j in range (len(carre_non_mag[i])) :
-        print (carre_non_mag[i][j], end= " ")
-    print ()
+#Question8
+def estcarremagique(carre):
+  return testlignesegales(carre) == testcolonnes(carre) and testlignesegales(carre) == testdiagonalesegales(carre) and testlignesegales(carre) != -1
+print(estcarremagique(carre_mag))
+print(estcarremagique(carre_pas_mag))
 
-#Exercice 4
-def afficheCarre(carre):
-    """ Affiche la liste à 2 dimensions carre comme un carré"""
-    pass
-
-afficheCarre(carre_mag)
-afficheCarre(carre_pas_mag)
-
-#Exercice 5
-def testLignesEgales(carre):
-    """ Renvoie la somme des éléments d'une ligne de la liste 2D carre si toutes les lignes ont la même somme, et -1 sinon """
-    pass
-
-print(testLignesEgales(carre_mag))
-print(testLignesEgales(carre_pas_mag))
-
-#Exercice 6
-def testColonnesEgales(carre):
-    """ Renvoie la somme des éléments d'une colonne de la liste 2D carre si toutes les colonnes ont la même somme, et -1 sinon """
-    pass
-
-print(testColonnesEgales(carre_mag))
-print(testColonnesEgales(carre_pas_mag))
-
-#Exercice 7
-def testDiagonalesEgales(carre):
-    """ Renvoie la somme des éléments d'une diagonale de la liste 2D carre si les 2 diagonales ont la même somme, et -1 sinon """
-    pass
-
-print(testDiagonalesEgales(carre_mag))
-print(testDiagonalesEgales(carre_pas_mag))
-
-#Exercice 8
-def estCarreMagique(carre):
-    """ Renvoie True si c'est un carre magique et False sinon"""
-    pass
-
-print(estCarreMagique(carre_mag))
-print(estCarreMagique(carre_pas_mag))
-
-#Exercice 9
-def estNormal(carre):
-    """ Retourne True si contient toutes les valeurs de 1 à n^2 où n est la taille 
-        du carré, et False sinon """
-    pass
-
-print(estNormal(carre_mag))
-print(estNormal(carre_pas_mag))
+#Question9
+def estnormal(carre):
+  liste = []
+  for lignes in carre :
+    liste.extend(lignes)
+  taille = len(carre)
+  for entier in range (1, taille * taille + 1) :
+    if entier not in liste :
+      return False
+  return True
+print(estnormal(carre_mag))
+print(estnormal(carre_pas_mag))
--- exercises/TD04_listes/test.py
+++ exercises/TD04_listes/test.py
@@ -1,6 +1,32 @@
-def ma_fonction(pays, age = 1, nom = "toto"):
-    print(nom," a ", age, "ans et vit en ", pays)
-    
-ma_fonction("france")
-ma_fonction("allemagne", 18, "kurt")
-ma_fonction("italie", 77)
+def syracuse (n):
+  L = [n]
+  while n != 1:
+    if n % 2 == 0:
+      n //= 2
+    else :
+      n = (n + 3 + 1)
+    L.append(n)
+  return L 
+print(syracuse(3))
+
+def testeConjecture(n_max):
+  for i in range (1, n_max +1):
+    syracuse (i)
+  return True
+print(testeConjecture(10000))
+
+def temps_vol(n):
+  return len (syracuse(n)) - 1
+def tempsVolListe(n_max):
+  return temps_vol(i) #\ for i in range (1, n_max + 1)]
+L = tempsVolListe(10000)
+t_max = max(L)
+print ("l'entier", L.index (t_max)+1, "a le plus grand temps", t_max)
+
+def alt_max(n):
+  return max(syracuse(n))
+def alt_max_liste (alt_max):
+  return [alt_max(i) for i in range (1, alt_max + 1)]
+L_alt = alt_max_liste(10000)
+b= max(L_alt)
+print("l'entier[...]")
